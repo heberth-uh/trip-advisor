5e4c7895a9396143b3ccefe1d0de1aa74b6e7d41
heberth-uh <heberth.uh@gmail.com>
2024-12-28 17:49:24 -0600

    feat: Add responsive styles in PopularCities section

--- src/components/Home/CityCard.jsx
+++ src/components/Home/CityCard.jsx
@@ -8,13 +8,13 @@ export default function CityCard({ city }) {
     const navigate = useNavigate()
     const { type } = useContext(MainContext)
     return (
-        <div className={`rounded-3xl overflow-hidden min-w-36 min-h-36 bg-cover bg-[20%] relative aspect-square cursor-pointer`}
+        <div className={`rounded-3xl lg:rounded-2xl overflow-hidden min-w-36 min-h-36 bg-cover bg-[20%] relative aspect-square cursor-pointer`}
             style={{ backgroundImage: `url('${city.imageUrl}')` }}
             onClick={() => navigate(`/${type}/results/location/${city.location_id}`)}>
             <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black/0 via-black/10 to-black/60" />
             <div className="absolute left-0 bottom-0">
-                <p className="flex items-start gap-1 pl-3 pr-2 pb-3 text-white text-sm font-medium">
-                    <span className="text-xs">
+                <p className="flex items-start gap-1 pl-3 pr-2 pb-3 md:px-5 md:pb-5 text-white text-xs md:text-base font-medium">
+                    <span className="text-xs md:text-base">
                         <IoLocationSharp />
                     </span>
                     <span className="leading-none">
--- src/components/Home/PopularCities.jsx
+++ src/components/Home/PopularCities.jsx
@@ -6,12 +6,14 @@ export default function PopularCities() {
     const { popularCities } = useContext(MainContext)
     return (
         <section className="px-4 md:px-0 mt-10 bg-lighter-gray">
-            <div className="container py-8">
-                <h2 className="text-xl font-semibold text-primary text-center pb-8">Popular Cities</h2>
-                <div className="flex justify-start items-center gap-3 overflow-x-auto scrollbar-invisible">
+            <div className="container py-8 lg:py-16 md:px-10 mx-auto lg:w-3/4 xl:w-3/5 2xl:w-1/2">
+                <h2 className="text-xl lg:text-[2rem] font-semibold text-primary text-center pb-8">
+                    Popular Cities
+                </h2>
+                <div className="flex items-center justify-start gap-3 md:grid md:grid-cols-2 md:gap-5 lg:grid-cols-3 lg:gap-8 overflow-x-auto scrollbar-invisible">
                     {
-                        popularCities.map( city => (
-                            <CityCard key={city.id} city={city}/>
+                        popularCities.map(city => (
+                            <CityCard key={city.id} city={city} />
                         ))
                     }
                 </div>
