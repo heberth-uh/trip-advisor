a31942ff597a229210f9cc611f6017fc8defb9a1
heberth-uh <heberth.uh@gmail.com>
2024-12-31 22:09:11 -0600

    style: Add spacing in LocationResult

--- src/components/LocationResults.jsx
+++ src/components/LocationResults.jsx
@@ -7,18 +7,21 @@ import Error from "./Error"
 import NoResults from "./NoResults"
 import Dropdown from "./common/Dropdown"
 
-function LocationList({ locations }) {
-    const {sort, setSort, sortList} = useContext(MainContext)
+function LocationList({ locations, query }) {
+    const { sort, setSort, sortList } = useContext(MainContext)
     return (
         <div>
-            <div>
+            <div className="flex flex-col gap-y-3 mb-4">
+                <h1 className="text-lg font-semibold">
+                    Results from <span className="italic font-bold">"{query}"</span>
+                </h1>
                 <Dropdown options={sortList} defaultValue={sort} handlerValue={setSort} />
             </div>
-            <section>
+            <div>
                 {locations.map(({ result_object }) => (
                     <LocationCard location={result_object} key={result_object.location_id} />
                 ))}
-            </section>
+            </div>
         </div>
     )
 }
@@ -27,13 +30,13 @@ export function LocationResults({ locations, isFirstSearch, error }) {
     const searchParam = useURLParam('search')
     const message = `No results found for "${searchParam}"`
     return (
-        <section>
+        <section className="px-4 pt-4 pb-16 mx-auto container">
             {
                 error && <Error>{error}</Error>
             }
             {
                 locations?.length > 0
-                    ? <LocationList locations={locations} />
+                    ? <LocationList locations={locations} query={searchParam} />
                     : !isFirstSearch.current && <NoResults message={message} />
             }
         </section>
