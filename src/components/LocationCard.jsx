e764cdf1002e53bef905c3b30b79c1a7bd656827
heberth-uh <heberth.uh@gmail.com>
2025-01-01 22:35:28 -0600

    style: Add responsive styles in LocationCard component

--- src/components/LocationCard.jsx
+++ src/components/LocationCard.jsx
@@ -11,8 +11,8 @@ export default function LocationCard({ location }) {
     const navigate = useNavigate()
 
     return (
-        <div className="flex gap-3 cursor-pointer" onClick={() => navigate(`/${type}/results/location/${location.location_id}`)}>
-            <div className="rounded-2xl lg:rounded-2xl overflow-hidden min-w-36 min-h-36 bg-cover bg-no-repeat bg-[20%] relative aspect-square"
+        <div className="flex gap-3 lg:gap-4 cursor-pointer" onClick={() => navigate(`/${type}/results/location/${location.location_id}`)}>
+            <div className="rounded-2xl lg:rounded-2xl overflow-hidden min-w-36 min-h-36 lg:min-w-44 lg:min-h-44 bg-cover bg-no-repeat bg-[20%] relative aspect-square"
                 style={{ backgroundImage: `url('${location.photo?.images?.large.url}')` }}>
                 <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black/0 via-black/10 to-black/60" />
                 <div className="absolute bottom-0 left-0">
@@ -23,14 +23,14 @@ export default function LocationCard({ location }) {
                 </div>
             </div>
             <div className="flex flex-col justify-start gap-1">
-                <h4 className="text-base font-semibold">
+                <h4 className="text-base lg:text-lg font-semibold">
                     {location.name}
                 </h4>
-                <div className="flex items-start gap-1 text-sm font-light">
-                    <span className="self-start">
+                <div className="flex items-start gap-1 font-light">
+                    <span className="self-start lg:self-center">
                         <IoLocationSharp />
                     </span>
-                    <p className="leading-none">
+                    <p className="text-sm lg:text-base leading-none">
                         {location.location_string}
                     </p>
                 </div>
