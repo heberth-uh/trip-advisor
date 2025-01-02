38e9ee1f1beb5f6a6648c054cd180899a2f9ecc2
heberth-uh <heberth.uh@gmail.com>
2025-01-01 23:12:56 -0600

    styles: Fix syles in dropwdown position in mobile view

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ children, handleOnclick, Icon }) {
     return (
         <button
             onClick={handleOnclick}
-            className="flex items-center justify-center bg-secondary hover:bg-light-gray text-center rounded-full py-[6px] lg:py-[10px] pl-4 pr-3 lg:pl-5 lg:pr-4">
+            className="relative flex items-center justify-center bg-secondary hover:bg-light-gray text-center rounded-full py-[6px] lg:py-[10px] pl-4 pr-3 lg:pl-5 lg:pr-4">
             <span className="flex items-center justify-center text-sm lg:text-base font-medium lg:font-semibold uppercase gap-1">
                 {children}
                 {
--- src/components/common/Dropdown.jsx
+++ src/components/common/Dropdown.jsx
@@ -31,28 +31,28 @@ export default function Dropdown({ children, options, defaultValue, handlerValue
     }, [showDropdown, dropdownRef])
 
     return (
-        <div ref={dropdownRef} className="relative">
+        <div ref={dropdownRef}>
             <Button handleOnclick={handleClickDropdown} Icon={IoIosArrowDown}>
                 {defaultOptionValue}
                 <span className="text-lg">
                     {children}
                 </span>
+                <ul className={`bg-white border-2 border-light-gray py-1 rounded-xl mt-[2px] overflow-hidden z-50 ${showDropdown ? 'absolute top-full right-0 lg:right-auto' : 'hidden'}`}>
+                    {
+                        options.map(option => (
+                            <li
+                                key={option.id}
+                                onClick={() => handleChangeValue(option.value)}
+                                className={`flex justify-between items-center gap-1 hover:bg-gray-200 px-4 py-[6px] text-sm font-normal cursor-pointer text-nowrap ${option.value === defaultValue && 'bg-gray-100 font-semibold'}`}>
+                                {option.name}
+                                <span className={`text-xs ${option.value === defaultValue ? 'block' : 'hidden'}`}>
+                                    <FaCheck />
+                                </span>
+                            </li>
+                        ))
+                    }
+                </ul>
             </Button>
-            <ul className={`bg-white border-2 border-light-gray py-1 rounded-xl mt-[2px] overflow-hidden z-50 ${showDropdown ? 'absolute top-full right-0 lg:right-auto' : 'hidden'}`}>
-                {
-                    options.map(option => (
-                        <li
-                            key={option.id}
-                            onClick={() => handleChangeValue(option.value)}
-                            className={`flex justify-between items-center gap-1 hover:bg-gray-200 px-4 py-[6px] text-sm cursor-pointer text-nowrap ${option.value === defaultValue && 'bg-gray-100 font-medium'}`}>
-                            {option.name}
-                            <span className={`text-xs ${option.value === defaultValue ? 'block' : 'hidden'}`}>
-                                <FaCheck />
-                            </span>
-                        </li>
-                    ))
-                }
-            </ul>
         </div>
     )
 }
