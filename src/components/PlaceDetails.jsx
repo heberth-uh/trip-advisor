640c5a1c776c3813234558dc03948bc51e1cd86a
heberth-uh <heberth.uh@gmail.com>
2025-01-12 19:11:44 -0600

    style: Add styles for header and aobut in PlaceDetails

--- src/components/PlaceDetails.jsx
+++ src/components/PlaceDetails.jsx
@@ -10,6 +10,9 @@ import Cuisine from "./Cuisine"
 import Tag from "./common/Tag"
 import Booking from "./Booking"
 import Offers from "./Offers"
+import StarsRating from "./widgets/StarsRating"
+// Icons
+import { PiClockCountdownBold } from "react-icons/pi";
 
 export default function ({ place }) {
     const params = useParams()
@@ -20,40 +23,60 @@ export default function ({ place }) {
     return (
         <section>
             <section>
-                <h1>{place.name}</h1> <span>{place.ranking}</span>
-                <p>{place.rating}</p>
+                <div className="flex flex-col gap-y-2 lg:flex-row lg:justify-between">
+                    <div className="flex justify-between items-center gap-x-3">
+                        <h1 className="text-lg font-semibold">
+                            {place.name}
+                        </h1>
+                        <div className="flex">
+                            {place.open_now_text &&
+                                <Tag bg={'bg-highlight'}>
+                                    {place.open_now_text}
+                                </Tag>
+                            }
+                        </div>
+                        <span className="text-base hidden lg:block">{place.ranking}</span>
+                    </div>
+
+                    <div className="flex justify-between items-center">
+                        <StarsRating rate={place.rating} />
+                        <span className="text-sm lg:hidden">{place.ranking}</span>
+                    </div>
+                </div>
+
                 <p>{place.price_level}</p>
-                <img src={place.photo?.images?.large.url} alt={place.name} />
+                <img src={place.photo?.images?.large.url} alt={place.name}
+                    className="rounded-3xl w-full" />
                 <p>{place.price}</p>
-                <p>{place.open_now_text}</p>
                 {place.recommended_visit_length &&
-                    <div>
-                        <p>{place.recommended_visit_length}</p>
+                    <div className="flex items-center gap-1 text-gray-500 text-sm">
+                        <PiClockCountdownBold />
+                        <p>Duration: {place.recommended_visit_length}</p>
                     </div>
                 }
             </section>
             <article>
-                <div>
-                    <h2>About</h2>
-                    <p>{place.description}</p>
-                </div>
+                <h2 className="font-semibold text-lg text-primary mb-2">About</h2>
+                <p className="text-base font-extralight leading-7 text-balance mb-2">
+                    {place.description}
+                </p>
                 {place.subcategory &&
-                    <div>
-                        <h3>Tags</h3>
-                        {
-                            place.subcategory && place.subcategory.map(subcategory => (
-                                <Tag key={subcategory.key}>{subcategory.name}</Tag>
-                            ))
-                        }
+                    <div className="flex gap-2">
+                        {place.subcategory && place.subcategory.map(subcategory => (
+                            <Tag key={subcategory.key}>#{subcategory.name}</Tag>
+                        ))}
                     </div>
                 }
-                <a href={place.write_review} target='_blank'>Want a write a review?</a>
+                {/* <a href={place.write_review} target='_blank'>Want a write a review?</a> */}
             </article>
             {
-                place.reserve_info && <Booking info={place.reserve_info}/>
+                place.reserve_info && <Booking info={place.reserve_info} />
             }
             <section>
-                <h2>More information</h2>
+                <h2 className="font-semibold text-lg text-primary mb-2">
+                    More information
+                </h2>
+
                 <p>address: <span>{place.address}</span></p>
                 <p>phone: <span>{place.phone}</span></p>
                 {place.website && <p>website: <span>{place.website}</span></p>}
@@ -92,7 +115,7 @@ export default function ({ place }) {
                 </div>
             }
             {
-                place.rating_histogram && <RatingHistogram ratings={place.rating_histogram} rating={place.rating} num_reviews={place.num_reviews}/>
+                place.rating_histogram && <RatingHistogram ratings={place.rating_histogram} rating={place.rating} num_reviews={place.num_reviews} />
             }
             <hr />
             {
--- src/components/common/Tag.jsx
+++ src/components/common/Tag.jsx
@@ -1,4 +1,4 @@
-export default function Tag({ children, bg }) {
+export default function Tag({ children, bg='bg-secondary' }) {
     return (
         <div className={`px-2 py-1 xl:py-[2px] rounded-md ${bg} text-xs xl:text-sm font-normal leading-none`}>
             {children}
