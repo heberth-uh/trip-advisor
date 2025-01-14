ffa2e6d00f13a193f82bfa09230d3e6966c9a5cc
heberth-uh <heberth.uh@gmail.com>
2025-01-13 23:41:10 -0600

    style: Add flex buttons in offers section

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,3 +1,4 @@
+import Button from "./common/Button"
 import TicketCard from "./TicketCard"
 
 export default function Offers({ offers }) {
@@ -6,20 +7,16 @@ export default function Offers({ offers }) {
 
     return (
         <section>
-            {
-                offers.offer_list &&
-                <div>
-                    <h2>Offers</h2>
-                    { offers.offer_list.map(offer => <TicketCard key={offer.product_code} ticket={offer} />) }
-                </div>
-            }
-            {
-                offers.ticket_list &&
-                <div>
-                    <h2>Tickets</h2>
-                    { offers.ticket_list.map(offer => <TicketCard key={offer.product_code} ticket={offer} />) }
-                </div>
-            }
+            <div className="flex flex-cols gap-1">
+                {
+                    offers.offer_list && <Button bg={'primary'}>Show Offers</Button>
+                    // {offers.offer_list.map(offer => <TicketCard key={offer.product_code} ticket={offer} />)}
+                }
+                {
+                    offers.ticket_list && <Button bg={'primary'}>Show Tickets</Button>
+                    // {offers.ticket_list.map(offer => <TicketCard key={offer.product_code} ticket={offer} />)}
+                }
+            </div>
         </section>
     )
 }
