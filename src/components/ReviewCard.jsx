c4d17db45de98e94cc68065cae052134874c3e69
heberth-uh <heberth.uh@gmail.com>
2025-01-28 23:39:18 -0600

    feat: shorten text in reviewCard

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import Gallery from "./Gallery"
 import StarsRating from "./widgets/StarsRating"
 // Icons
 import { AiFillLike } from "react-icons/ai";
+import { FaAngleDown } from "react-icons/fa";
+import { FaAngleUp } from "react-icons/fa6";
 
 export default function ReviewCard({ review }) {
 
+    const [isExpanded, setIsExpanded] = useState(false)
     // this method need to be callable everywhere (hook/useState)
     function formatDate(stringDate) {
         let newDate = new Date(stringDate)
         return newDate.toDateString()
+    }
+
+    const shortenText = (text, length) => {
+        let textArray = text.split(' ')
+        if (textArray.length <= length) return text
+        return textArray.slice(0, length).join(' ') + '...'
     }
 
     return (
@@ -26,7 +36,18 @@ export default function ReviewCard({ review }) {
                     {review.title}
                 </h4>
                 <p className="text-base font-extralight leading-6">
-                    {review.text}
+                    {isExpanded ? review.text : shortenText(review.text, 30)}
+                    {
+                        review.text.split(' ').length > 30 ? isExpanded ?
+                            <span className="text-primary font-normal flex items-center">
+                                Read more <FaAngleDown />
+                            </span>
+                            :
+                            <span className="text-primary font-normal flex items-center">
+                                Read less <FaAngleUp />
+                            </span>
+                            : ''
+                    }
                 </p>
             </div>
             <div className="flex items-center justify-between flex-wrap text-[#747474] text-sm font-light">
@@ -43,11 +64,11 @@ export default function ReviewCard({ review }) {
             }
             {
                 review.owner_response &&
-                <div className="flex flex-col gap-y-2 border-l-4 border-lighter-gray pl-5 ml-10">
-                    <p className="text-sm font-medium">
+                <div className="flex flex-col gap-y-2 border-l-4 border-lighter-gray pl-5 ml-3">
+                    <p className="text-base font-medium">
                         {review.owner_response.responder}
                     </p>
-                    <p className="text-sm font-extralight leading-6">
+                    <p className="text-base font-extralight leading-6">
                         {review.owner_response.text}
                     </p>
                     <p className="text-[#747474] text-sm font-light text-end">
