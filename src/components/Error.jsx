55de6912c743b587088c7c5bb1ee0a5d6e0e4026
heberth-uh <heberth.uh@gmail.com>
2025-02-06 22:50:36 -0600

    style: Add styles in Error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,10 +1,21 @@
+import Heading3 from "./common/Heading3"
+import errorImg from "../../public/error-preview.png"
+
 export default function Error({ children }) {
     return (
         <div>
-            <i>
-                {children}
-            </i>
-            <hr />
+            <Heading3>Ops! Something went wrong</Heading3>
+            <p className="mt-2 font-light text-sm">
+                Unexpected Error, contact support.
+            </p>
+            <div className="bg-red-100 p-3 mt-4 rounded-md">
+                <i className="font-light text-sm">
+                    {children}
+                </i>
+            </div>
+            <div className="my-2 flex justify-center">
+                <img src={errorImg} className="h-60 w-60" alt="Error illustration" />
+            </div>
         </div>
     )
 }
