6a02047f959671ba2eba21908d9674d568460723
heberth-uh <heberth.uh@gmail.com>
2025-02-14 00:17:31 -0600

    style: Add animation in topAttractions Cards

--- src/components/Home/TopAttractions.jsx
+++ src/components/Home/TopAttractions.jsx
@@ -16,21 +16,25 @@ export function TopAttractions() {
                         popularAttractions.map(attraction => (
                             <a key={attraction.location_id}
                                 href={`/attractions/get-details/${attraction.location_id}`}
-                                className="flex-none sflex sflex-col max-w-[200px] md:max-w-full">
-                                <img src={attraction.photo?.images?.medium?.url} alt={attraction.photo?.caption}
-                                    className="rounded-lg w-[200px] h-[150px] md:w-full md:h-[200px] lg:h-[150px]" />
-                                <div className="flex flex-col md:flex-row md:justify-between items-start md:items-start mt-2">
-                                    <p className="text-sm md:text-base font-semibold leading-noned">
-                                        {attraction.name}
-                                    </p>
-                                    {attraction.rating && <div className="flex justify-center items-center gap-1">
-                                        <span className="text-highlight"><FaStar/></span>
-                                        <p className="text-sm md:text-base">{attraction.rating}</p>
-                                    </div>}
+                                className="flex-none rounded-lg overflow-hidden max-w-[200px] md:max-w-full h-full bg-gray-100">
+                                <div className="overflow-hidden">
+                                    <img src={attraction.photo?.images?.large?.url} alt={attraction.photo?.caption}
+                                        className="w-[200px] h-[150px] md:w-full md:h-[200px] lg:h-[150px] object-cover object-center scale-110 hover:scale-100 transition-all ease-out duration-500" />
                                 </div>
-                                <p className="text-xs md:text-sm font-normal">
-                                    {attraction.location_string}
-                                </p>
+                                <div className="py-2 px-3">
+                                    <div className="flex flex-col md:flex-row md:justify-between items-start md:items-start">
+                                        <p className="text-sm md:text-base font-semibold leading-noned">
+                                            {attraction.name}
+                                        </p>
+                                        {attraction.rating && <div className="flex justify-center items-center gap-1">
+                                            <span className="text-highlight"><FaStar/></span>
+                                            <p className="text-sm md:text-base">{attraction.rating}</p>
+                                        </div>}
+                                    </div>
+                                    <p className="text-xs md:text-sm font-normal">
+                                        {attraction.location_string}
+                                    </p>
+                                </div>
                             </a>
                         ))
                     }
