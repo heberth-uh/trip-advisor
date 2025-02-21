ddf7edf76c277cab38c77e3fb3a869e506db51d0
heberth-uh <heberth.uh@gmail.com>
2025-02-20 23:45:14 -0600

    fix: submit form on keypress enter in SearchForm

--- src/components/common/Dropdown.jsx
+++ src/components/common/Dropdown.jsx
@@ -32,27 +32,37 @@ export default function Dropdown({ children, options, defaultValue, handlerValue
 
     return (
         <div ref={dropdownRef}>
-            <Button handleOnclick={handleClickDropdown} Icon={IoIosArrowDown}>
-                {defaultOptionValue}
-                <span className="text-lg">
-                    {children}
-                </span>
-                <ul className={`bg-white border-2 border-light-gray py-1 rounded-xl mt-[2px] overflow-hidden z-50 ${showDropdown ? 'absolute top-full right-0 lg:right-auto' : 'hidden'}`}>
-                    {
-                        options.map(option => (
-                            <li
-                                key={option.id}
-                                onClick={() => handleChangeValue(option.value)}
-                                className={`flex justify-between items-center gap-1 hover:bg-gray-200 px-4 py-[6px] text-sm font-normal cursor-pointer text-nowrap ${option.value === defaultValue && 'bg-gray-100 font-semibold'}`}>
-                                {option.name}
-                                <span className={`text-xs ${option.value === defaultValue ? 'block' : 'hidden'}`}>
-                                    <FaCheck />
-                                </span>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </Button>
+            <div
+                onClick={handleClickDropdown}
+                onKeyDown={handleClickDropdown}
+                className="relative flex-1 flex items-center justify-center text-center rounded-full py-[6px] lg:py-[10px] pl-4 pr-3 lg:pl-5 lg:pr-4 bg-secondary hover:bg-light-gray text-black cursor-pointer"
+                role="button"
+                tabIndex={0}>
+                <div className="flex items-center justify-center text-sm lg:text-base font-medium lg:font-semibold uppercase gap-1">
+                    {defaultOptionValue}
+                    <span className="text-lg">
+                        {children}
+                    </span>
+                    <span className="text-lg">
+                    <IoIosArrowDown />
+                    </span>
+                    <ul className={`bg-white border-2 border-light-gray py-1 rounded-xl mt-[2px] overflow-hidden z-50 ${showDropdown ? 'absolute top-full right-0 lg:right-auto' : 'hidden'}`}>
+                        {
+                            options.map(option => (
+                                <li
+                                    key={option.id}
+                                    onClick={() => handleChangeValue(option.value)}
+                                    className={`flex justify-between items-center gap-1 hover:bg-gray-200 px-4 py-[6px] text-sm font-normal cursor-pointer text-nowrap ${option.value === defaultValue && 'bg-gray-100 font-semibold'}`}>
+                                    {option.name}
+                                    <span className={`text-xs ${option.value === defaultValue ? 'block' : 'hidden'}`}>
+                                        <FaCheck />
+                                    </span>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
