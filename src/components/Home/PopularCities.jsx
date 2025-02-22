24f2279a23a1606850204b78d2a1d19051735ca5
heberth-uh <heberth.uh@gmail.com>
2025-02-22 00:00:06 -0600

    feat: Add component heading1 in sectinos

--- src/components/Home/PopularCities.jsx
+++ src/components/Home/PopularCities.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import { MainContext } from "../../context/MainContext"
 // Components
 import ImagePlace from "../ImagePlace"
+import Heading1 from "../common/Heading1";
 // Icons
 import { IoLocationSharp } from "react-icons/io5";
 
@@ -10,9 +11,9 @@ export default function PopularCities() {
     return (
         <section className="px-4 md:px-0 bg-lighter-gray">
             <div className="container py-16 lg:py-16 md:px-10 mx-auto lg:w-3/4 xl:w-3/5 2xl:w-1/2">
-                <h2 className="text-xl md:text-2xl lg:text-[2rem] font-semibold text-primary text-center pb-6 lg:pb-8">
+                <Heading1>
                     Popular Cities
-                </h2>
+                </Heading1>
                 <div className="flex items-center justify-start gap-3 md:grid md:grid-cols-2 md:gap-5 lg:grid-cols-3 lg:gap-8 overflow-x-auto scrollbar-invisible">
                     {
                         popularCities.map(city => (
--- src/components/Home/TopAttractions.jsx
+++ src/components/Home/TopAttractions.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react"
 import { MainContext } from "../../context/MainContext"
+// Components
+import Heading1 from "../common/Heading1";
 // Icon
 import { FaStar } from "react-icons/fa";
 
@@ -8,9 +10,9 @@ export function TopAttractions() {
     return (
         <section className="px-4 md:px-0 bg-white">
             <div className="container py-16 lg:py-16 md:px-10 mx-auto lg:w-3/4 xl:w-3/5 2xl:w-1/2">
-                <h2 className="text-xl md:text-2xl lg:text-[2rem] font-semibold text-primary text-center pb-6 lg:pb-8">
+                <Heading1>
                     Top Attractions to visit
-                </h2>
+                </Heading1>
                 <div className="flex items-start justify-start gap-3 md:grid md:grid-cols-2 md:gap-5 lg:grid-cols-3 lg:gap-8 overflow-x-auto scrollbar-invisible">
                     {
                         popularAttractions.map(attraction => (
